b00ffc1f99dbb0cbfaa090200b8eb82a4df8a5ef
enriquea <enrique.audain@gmail.com>
2022-12-01 14:25:50 +0100

    added basic setup/requirements files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fsspark',
+    version='0.0.1',
+    packages=['fsspark', 'fsspark.fs', 'fsspark.utils'],
+    url='https://github.com/bigbio/fsspark',
+    license='Apache-2.0',
+    author='eam',
+    author_email='enrique.audain@gmail.com',
+    description='Feature selection in Spark'
+)
