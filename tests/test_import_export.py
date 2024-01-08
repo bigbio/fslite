32d7edf1892430d39fa65d7c6e2e23a68062e51a
enriquea <enrique.audain@gmail.com>
2024-01-08 23:39:29 +0100

    added test for import funcions

---
+++ tests/test_import_export.py
@@ -0,0 +1,45 @@
+import unittest
+
+import pyspark
+import pyspark.pandas as ps
+
+from fsspark.context import init_spark, stop_spark_session
+from fsspark.utils.datasets import get_tnbc_data_path
+from fsspark.utils.io import import_table, import_table_as_psdf
+
+
+class TestImportExport(unittest.TestCase):
+
+    def setUp(self) -> None:
+        init_spark(apply_pyarrow_settings=True,
+                   apply_extra_spark_settings=True,
+                   apply_pandas_settings=True)
+
+    def tearDown(self) -> None:
+        stop_spark_session()
+
+    def test_import_tsv(self):
+        """
+        Test import tsv file as Spark DataFrame.
+        :return: None
+        """
+        df = import_table(path=get_tnbc_data_path(),
+                          n_partitions=10)
+
+        self.assertIsInstance(df, pyspark.sql.DataFrame)
+        self.assertEqual(df.count(), 44)
+
+    def test_import_tsv_as_psdf(self):
+        """
+        Test import tsv file as Pandas on Spark DataFrame (PoS).
+        :return: None
+        """
+        df = import_table_as_psdf(path=get_tnbc_data_path(),
+                                  n_partitions=10)
+
+        self.assertIsInstance(df, ps.frame.DataFrame)
+        self.assertEqual(df.shape,  (44, 502))
+
+
+if __name__ == '__main__':
+    unittest.main()
