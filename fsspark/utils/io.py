d644b50eeaa188c1fecf7b0ed3c5f70ebd6f27d2
enriquea <enrique.audain@gmail.com>
2024-01-08 23:38:49 +0100

    minor change

--- fsspark/utils/io.py
+++ fsspark/utils/io.py
@@ -45,8 +45,8 @@ def import_table_as_psdf(path: str,
     """
     Import tsv file as Pandas on Spark DataFrame
 
-    :param path: File path
-    :param sep:
+    :param path: Path to TSV file
+    :param sep: Column separator (default: "\t")
     :param n_partitions: Minimal number of partitions
 
     :return: Pandas on Spark DataFrame
