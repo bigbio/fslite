150ae9323843ad697b54f02f7a8fb6fac71f01a3
enriquea <enrique.audain@gmail.com>
2024-01-10 22:25:45 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='fsspark',
     version='0.0.1',
-    packages=['fsspark', 'fsspark.fs', 'fsspark.utils'],
     url='https://github.com/bigbio/fsspark',
     license='Apache-2.0',
-    author='eam',
+    author='Enrique Audain Martinez',
     author_email='enrique.audain@gmail.com',
-    description='Feature selection in Spark'
+    description='Feature selection in Spark',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "pyspark",
+        "numpy",
+        "networkx",
+        "setuptools"
+    ],
+    classifiers=[
+        # Classifiers for your package
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.9.0',
 )
