6040a0d82a7b07553b2ea2c73056466c33d9de19
enriquea <enrique.audain@gmail.com>
2024-04-24 22:37:33 +0200

    added description for fs methods

--- fsspark/fs/constants.py
+++ fsspark/fs/constants.py
@@ -25,15 +25,29 @@ MULTIVARIATE_METHODS = {
 
 # Define machine learning wrapper methods constants
 
+# binary classification
 RF_BINARY = 'rf_binary'
-RF_MULTILABEL = 'rf_multilabel'
-RF_REGRESSION = 'rf_regression'
 LSVC_BINARY = 'lsvc_binary'
+FM_BINARY = 'fm_binary'  # TODO: implement this method
+
+# multilabel classification
+RF_MULTILABEL = 'rf_multilabel'
+LR_MULTILABEL = 'lg_multilabel'  # TODO: implement this method
+
+# regression
+RF_REGRESSION = 'rf_regression'
+FM_REGRESSION = 'fm_regression'  # TODO: implement this method
+
 
 # Define dict with machine learning wrapper methods and brief description
 ML_METHODS = {
     RF_BINARY: 'Random Forest Binary Classifier',
+    LSVC_BINARY: 'Linear SVC Binary Classifier',
+    FM_BINARY: 'Factorization Machine Binary Classifier',
+
     RF_MULTILABEL: 'Random Forest Multi-label Classifier',
+    LR_MULTILABEL: 'Logistic Regression Multi-label Classifier',
+
     RF_REGRESSION: 'Random Forest Regression',
-    LSVC_BINARY: 'Linear SVC Binary Classifier'
+    FM_REGRESSION: 'Factorization Machine Regression'
 }
