33634c425e030585250d640d56069b905b99e400
enriquea <enrique.audain@gmail.com>
2024-05-31 16:12:20 +0200

    support importing parquet files

--- fsspark/utils/io.py
+++ fsspark/utils/io.py
@@ -16,7 +16,7 @@ def import_table(path: str,
     Import tsv file as Spark DataFrame.
 
     :param path: File path
-    :param header:
+    :param header: True if the first row is header.
     :param sep: Column separator
     :param n_partitions: Minimal number of partitions
 
@@ -35,6 +35,31 @@ def import_table(path: str,
            .option("inferSchema", "true")
            .csv(path)
            .repartition(n_partitions)
+           )
+    return sdf
+
+
+def import_parquet(path: str,
+                   header: bool = True) -> pyspark.sql.DataFrame:
+    """
+    Import parquet file as Spark DataFrame.
+
+    :param path: File path
+    :param header: True if the first row is header.
+
+    :return: Spark DataFrame
+    """
+
+    _sc = pyspark.sql.SparkSession.getActiveSession()
+
+    if _sc is None:
+        raise ValueError("Active Spark Session not found...")
+
+    sdf = (_sc
+           .read
+           .option("header", header)
+           .option("inferSchema", "true")
+           .parquet(path)
            )
     return sdf
 
