c2312c899723e9d7a2f1db249507fdeea57e6de7
Yasset Perez-Riverol <ypriverol@gmail.com>
2024-09-22 09:08:03 +0100

    better structure for methods in constants.py

--- fsspark/fs/constants.py
+++ fsspark/fs/constants.py
@@ -59,3 +59,26 @@ FS_METHODS = {
         ]
     }
 }
+
+
+def get_fs_methods():
+    """
+    Get the list of feature selection methods
+    :return: dict
+    """
+    return FS_METHODS
+
+def get_fs_method_details(method_name: str):
+    """
+    Get the details of the feature selection method, this function search in all-methods definitions
+    and get the details of the method with the given name. If the method is not found, it returns None.
+    The method name is case-insensitive.
+    :param method_name: str
+    :return: dict
+    """
+
+    for method_type in FS_METHODS:
+        for method in FS_METHODS[method_type]['methods']:
+            if method['name'].lower() == method_name.lower():
+                return method
+    return None
