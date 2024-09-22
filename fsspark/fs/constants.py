032a422eb6c905ece64733bce8a7fb31440abb0a
Yasset Perez-Riverol <ypriverol@gmail.com>
2024-09-22 09:05:41 +0100

    better structure for methods in constants.py

--- fsspark/fs/constants.py
+++ fsspark/fs/constants.py
@@ -1,53 +1,61 @@
-# Define constants for the project
+"""
+This file contains a list of constants used in the feature selection and machine learning methods.
+"""
 
-
-# Define univariate feature selection methods constants
-ANOVA = 'anova'
-UNIVARIATE_CORRELATION = 'u_corr'
-F_REGRESSION = 'f_regression'
-
-# Define dict with univariate feature selection methods and brief description
-UNIVARIATE_METHODS = {
-    ANOVA: 'ANOVA univariate feature selection (F-classification)',
-    UNIVARIATE_CORRELATION: 'Univariate Correlation',
-    F_REGRESSION: 'Univariate F-regression'
-}
-
-# Define multivariate feature selection methods constants
-MULTIVARIATE_CORRELATION = 'm_corr'
-MULTIVARIATE_VARIANCE = 'variance'
-
-# Define dict with multivariate feature selection methods and brief description
-MULTIVARIATE_METHODS = {
-    MULTIVARIATE_CORRELATION: 'Multivariate Correlation',
-    MULTIVARIATE_VARIANCE: 'Multivariate Variance'
-}
-
-# Define machine learning wrapper methods constants
-
-# binary classification
-RF_BINARY = 'rf_binary'
-LSVC_BINARY = 'lsvc_binary'
-FM_BINARY = 'fm_binary'  # TODO: implement this method
-
-# multilabel classification
-RF_MULTILABEL = 'rf_multilabel'
-LR_MULTILABEL = 'lg_multilabel'  # TODO: implement this method
-
-# regression
-RF_REGRESSION = 'rf_regression'
-FM_REGRESSION = 'fm_regression'  # TODO: implement this method
-
-
-# Define dict with machine learning wrapper methods and brief description
-ML_METHODS = {
-    RF_BINARY: 'Random Forest Binary Classifier',
-    LSVC_BINARY: 'Linear SVC Binary Classifier',
-    FM_BINARY: 'Factorization Machine Binary Classifier',
-
-    RF_MULTILABEL: 'Random Forest Multi-label Classifier',
-    LR_MULTILABEL: 'Logistic Regression Multi-label Classifier',
-
-    RF_REGRESSION: 'Random Forest Regression',
-    FM_REGRESSION: 'Factorization Machine Regression'
+FS_METHODS = {
+    'univariate': {
+        "title": 'Univariate Feature Selection',
+        "methods": [
+            {
+                'name': 'anova',
+                'description': 'ANOVA univariate feature selection (F-classification)'
+            }
+        ]
+    },
+    'multivariate': {
+        "title": 'Multivariate Feature Selection',
+        "methods": [
+            {
+                'name': 'm_corr',
+                'description': 'Multivariate Correlation'
+            },
+            {
+                'name': 'variance',
+                'description': 'Multivariate Variance'
+            }
+        ]
+    },
+    'ml': {
+        "title": 'Machine Learning Wrapper',
+        "methods": [
+            {
+                'name': 'rf_binary',
+                'description': 'Random Forest Binary Classifier'
+            },
+            {
+                'name': 'lsvc_binary',
+                'description': 'Linear SVC Binary Classifier'
+            },
+            {
+                'name': 'fm_binary',
+                'description': 'Factorization Machine Binary Classifier'
+            },
+            {
+                'name': 'rf_multilabel',
+                'description': 'Random Forest Multi-label Classifier'
+            },
+            {
+                'name': 'lg_multilabel',
+                'description': 'Logistic Regression Multi-label Classifier'
+            },
+            {
+                'name': 'rf_regression',
+                'description': 'Random Forest Regression'
+            },
+            {
+                'name': 'fm_regression',
+                'description': 'Factorization Machine Regression'
+            }
+        ]
+    }
 }
